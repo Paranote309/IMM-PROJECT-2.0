49ab9148e377996185427a1aee6ac258ded81db3
IrmDam <92793460+IrmDam@users.noreply.github.com>
2021-12-12 21:29:38 +0000

    N/A

--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -7,7 +7,7 @@ public class DetectCollisions : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,9 +17,9 @@ public class DetectCollisions : MonoBehaviour
     }
 
     void onTriggerEnter(Collider other){
-        if(gameObject != gameObject(Ball))
-         {
-             Destroy(other.gameObject);
-         }
+    //    if(other.gameObject.name != "Ball")
+     //   {
+            Destroy(gameObject);
+      //  }
     }
 }
